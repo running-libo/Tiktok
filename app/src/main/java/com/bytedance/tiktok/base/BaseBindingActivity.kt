856f7a74cd413f921d40b82abc7a63ae5cd1d9d6
libo <417845639@qq.com>
2023-12-20 01:17:11 +0800

    basefragment baseactivity添加

---
+++ app/src/main/java/com/bytedance/tiktok/base/BaseBindingActivity.kt
@@ -0,0 +1,28 @@
+package com.bytedance.tiktok.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+/**
+ * 支持View Binding Activity
+ */
+abstract class BaseBindingActivity<VB : ViewBinding>(
+    val block: (LayoutInflater) -> VB
+) : AppCompatActivity() {
+    private var _binding: VB? = null
+    protected val binding: VB
+        get() = requireNotNull(_binding) { "The property of binding has been destroyed." }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        _binding = block(layoutInflater)
+        setContentView(binding.root)
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/bytedance/tiktok/base/BaseBindingFragment.kt
@@ -0,0 +1,28 @@
+package com.bytedance.tiktok.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseBindingFragment<VB : ViewBinding>(
+    val block: (LayoutInflater) -> VB
+) : Fragment() {
+    private var _binding: VB? = null
+    protected val binding: VB
+        get() = requireNotNull(_binding) { "The property of binding has been destroyed." }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = block(layoutInflater)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
